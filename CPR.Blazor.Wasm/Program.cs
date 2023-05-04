ff8f5e8bc6d9db9ba445d3b4e391445a02f56a91
Luka Lai 賴育暉 <lukalai@starlux-airlines.com>
2023-05-04 15:37:01 +0800

    feat(BlazorSample): blazor展示模板

---
+++ CPR.Blazor.Wasm/Program.cs
@@ -0,0 +1,31 @@
+using CPR.Blazor.Wasm;
+using CPR.Blazor.Wasm.Settings;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+HttpClient httpClient = new HttpClient()
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+};
+builder.Services.AddScoped(sp => httpClient);
+await builder.Configuration.LoadSettingJson(httpClient);
+
+builder.Services.AddMudServices();
+builder.Services.AddGraphClient();
+builder.SetMsAuth();
+
+//»y¨t
+builder.Services.AddLocalization(option =>
+{
+    option.ResourcesPath = "Resources";
+});
+
+var host = builder.Build();
+//»y¨t
+await host.SetCulture();
+await host.RunAsync();
